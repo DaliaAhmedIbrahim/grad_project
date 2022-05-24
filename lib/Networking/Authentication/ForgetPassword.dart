7e36d5a9f7e4537b2282da481f662fcd55bc2b52
Salma Hany Farh <51351106+salmahanyfarh@users.noreply.github.com>
2022-05-24 22:16:26 +0200

    adding authentication

--- lib/Networking/Authentication/ForgetPassword.dart
+++ lib/Networking/Authentication/ForgetPassword.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:grad_project/layout/home_layout.dart';
 import 'package:http/http.dart' as http;
 
 class ApiForget {
